7f4a48c59a99367d672b2f2e5f3029868b52884a
3B032127 <{3B032127@gm.student.ncut.edu.tw}>
2023-11-28 11:23:41 +0800

    answer route URL、HTTP method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Products', ProductController::class);
+/*
+  products.index
+  - url；Products
+  - http_method：GET|HEAD
+  - conn_controller_method：ProductController@index
+
+  products.show
+  - url；Products/{Product}
+  - http_method：GET|HEAD
+  - conn_controller_method：ProductController@show
+
+  products.create
+  - url；Products/create
+  - http_method：GET|HEAD
+  - conn_controller_method：ProductController@create
+
+  products.store
+  - url；Products
+  - http_method：POST
+  - conn_controller_method：ProductController@store
+
+  products.edit
+  - url；Products/{Product}/edit
+  - http_method：GET|HEAD
+  - conn_controller_method：ProductController@edit
+
+  products.update
+  - url；Products/{Product}
+  - http_method：PUT|PATCH
+  - conn_controller_method：ProductController@update
+
+  products.destroy
+  - url；Products/{Product}
+  - http_method：DELETE
+  - conn_controller_method：ProductController@destroy
+
+ */
