468f896b451b227319d4f844d138f7eb95e4a880
3B032127 <{3B032127@gm.student.ncut.edu.tw}>
2023-12-06 14:38:37 +0800

    install laravel breeze and function

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +18,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::resource('Products', ProductController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 /*
+Route::resource('Products',ProductController::class);
+
 Route::resource('Products',ProductController::class)->only([
     'index','show','store','update','destroy'
 ]);
 */
-
+//Products
 Route::get('/', [ProductController::class, 'index'])->name("product.index");
 Route::get('Products', [ProductController::class, 'index'])->name("products.index");
 Route::get('Products/create', [ProductController::class, 'create'])->name("products.create");
@@ -33,45 +46,36 @@ Route::get('Products/{product}/edit', [ProductController::class, 'edit'])->name(
 Route::patch('Products/{product}', [ProductController::class, 'update'])->name("products.update");
 Route::delete('Products/{product}', [ProductController::class, 'destroy'])->name("products.destroy");
 
-
 /*
   products.index
   - url；Products
   - http_method：GET|HEAD
   - conn_controller_method：ProductController@index
-
   products.show
   - url；Products/{Product}
   - http_method：GET|HEAD
   - conn_controller_method：ProductController@show
-
   products.create
   - url；Products/create
   - http_method：GET|HEAD
   - conn_controller_method：ProductController@create
-
   products.store
   - url；Products
   - http_method：POST
   - conn_controller_method：ProductController@store
-
   products.edit
   - url；Products/{Product}/edit
   - http_method：GET|HEAD
   - conn_controller_method：ProductController@edit
-
   products.update
   - url；Products/{Product}
   - http_method：PUT|PATCH
   - conn_controller_method：ProductController@update
-
   products.destroy
   - url；Products/{Product}
   - http_method：DELETE
   - conn_controller_method：ProductController@destroy
-
  */
-
 /*
 Blog lara_0901 eshop 路由比較
 Blog admin開頭route URL
@@ -94,7 +98,6 @@ Blog admin開頭route URL
   使用 prefix 方法可以给组中的每个路由添加指定的 URI 前缀，常見用途是做權限管理
   eshop專案 則可直接透過指定url進入
 
-
   列出7個Products路由應該有的作用：
   products.index
   顯示目前有的產品資料
@@ -104,6 +107,7 @@ Blog admin開頭route URL
 
   products.create
   顯示創建產品資料的資料表
+
    products.store
   將create的表單產品資料新增至資料庫中
 
@@ -115,5 +119,4 @@ Blog admin開頭route URL
 
   products.destroy
   用於刪除指定的現有產品資料
-
  */
